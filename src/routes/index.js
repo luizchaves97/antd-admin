6df71fe15c783a85a58a44bc51802eabbc16c6b8
Luiz Felipe Rodrigues Chaves <luizchaves@Luizs-MacBook-Pro.local>
2019-07-17 01:37:52 -0300

    feat: remove BrowserRoute and set NODE_ENV on docker-compose.yml

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Switch } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Route from './Route';
 
 import SignInPage from '~/pages/SignIn';
-
 import DashboardPage from '~/pages/Dashboard';
 
 export default function Routes() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={SignInPage} />
-
-        <Route path="/dashboard" component={DashboardPage} isPrivate />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={SignInPage} />
+      <Route path="/dashboard" component={DashboardPage} isPrivate />
+    </Switch>
   );
 }
